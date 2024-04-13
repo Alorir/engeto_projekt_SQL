91356494067ef1100cb53969f4dd1ebf830116d9
Alorir <alena.kurkova88@gmail.com>
2024-04-13 13:59:28 +0200

    secondary table adding columns

--- secondary_table.sql
+++ secondary_table.sql
@@ -1,5 +1,7 @@
 CREATE TABLE t_alena_kurkova_project_sql_secondary_final AS
 SELECT 	e.country,
+		e.gini,
+		e.population,
 		e.year AS obs_year, 
 		e.GDP AS gdp
 FROM economies e
@@ -7,4 +9,5 @@ JOIN countries c ON e.country = c.country
 WHERE gdp IS NOT NULL
 AND c.continent = 'Europe'
 AND e.year BETWEEN 2006 AND 2018;
+
 
